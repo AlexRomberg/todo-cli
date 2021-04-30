cbd80c8a3f133d8837b189bd6f55939f1b1b6627
Alexander Romberg <adze@gmx.ch>
2021-04-30 23:35:48 +0200

    added add and delete command

---
+++ todo-cli/CInput.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace todo_cli {
+    class CInput {
+        public string getNewItemText(string[] args) {
+            if (args.Length > 1) {
+                return getArgumentInput(args);
+            } else {
+                Console.Write("Please enter a text of the item: ");
+                string itemText = Console.ReadLine();
+                if (showDialog("Save text?", true)) {
+                    return itemText;
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        public int getRemoveId(string[] args, CStorage storage) {
+            int id;
+            if (args.Length > 1) {
+                id = Convert.ToInt32(getArgumentInput(args));
+            } else {
+                Console.Write("Please enter the index of the item: ");
+                id = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (showDialog("Realy delete: \"" + storage.getTodoList()[id].Text + "\"?", true)) {
+                return id;
+            } else {
+                return -1;
+            }
+        }
+
+        private string getArgumentInput(string[] args) {
+            string text = "";
+            for (int i = 1; i < args.Length; i++) {
+                if (i > 1) {
+                    text += " ";
+                }
+                text += args[i];
+            }
+            return text;
+        }
+
+        private bool showDialog(string question, bool defaultAnswer) {
+            Console.Write("{0} {1}", question, (defaultAnswer ? "[Y/n]" : "[y/N]"));
+            while (true) {
+                switch (Console.ReadKey().Key) {
+                    case ConsoleKey.N:
+                        Console.WriteLine();
+                        return false;
+                    case ConsoleKey.Y:
+                    case ConsoleKey.J:
+                        Console.WriteLine();
+                        return true;
+                    case ConsoleKey.Enter:
+                        Console.WriteLine();
+                        return defaultAnswer;
+                }
+            }
+        }
+    }
+}
--- todo-cli/Program.cs
+++ todo-cli/Program.cs
@@ -5,28 +5,38 @@ namespace todo_cli {
     class Program {
         private static CStorage Storage;
         private static COutput Output;
+        private static CInput Input;
 
         static void Main(string[] args) {
             Storage = new CStorage();
             Output = new COutput();
+            Input = new CInput();
 
             if (args.Length > 0) {
-                //switch (args[0]) {
-                //    case "add":
-                //    case "-a":
-                //    case "a":
-                //        Storage.createElement("Hello");
-                //        break;
-                //    case "delete":
-                //    case "del":
-                //    case "-d":
-                //    case "d":
-                //        Storage.deleteElement(0);
-                //        break;
-                //    default:
-                //        Output.writeList(Storage.getTodoList());
-                //        break;
-                //}
+                switch (args[0].Replace("-", "")) {
+                    case "add":
+                    case "a":
+                    case "new":
+                    case "n":
+                        string itemText = Input.getNewItemText(args);
+                        if (itemText != null) {
+                            Storage.createElement(itemText);
+                            Output.writeList(Storage.getTodoList());
+                        }
+                        break;
+                    case "delete":
+                    case "del":
+                    case "d":
+                        int removeId = Input.getRemoveId(args, Storage);
+                        if (removeId >= 0) {
+                            Storage.deleteElement(removeId);
+                            Output.writeList(Storage.getTodoList());
+                        }
+                        break;
+                    default:
+                        Output.writeList(Storage.getTodoList());
+                        break;
+                }
             } else {
                 Output.writeList(Storage.getTodoList());
             }
