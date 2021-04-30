d2163a04eb39a131e2b31b1964641f0bc07887d6
Alexander Romberg <adze@gmx.ch>
2021-04-30 22:39:53 +0200

    added list-output

---
+++ todo-cli/COutput.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace todo_cli {
+    class COutput {
+        public void writeList(TodoElement[] todoList) {
+            string line = getTextLine();
+            int index = 0;
+
+            Console.WriteLine("  Id ╷ Status    ╷ Text\n" +
+                              "─────┼───────────┼{0}─", line);
+
+            foreach (TodoElement element in todoList) {
+                if (index > 0) {
+                    Console.WriteLine("─────┼───────────┼{0}─", line);
+                }
+
+                Console.WriteLine(" {0} │ {1} │ {2}", index.ToString().PadLeft(3), getStatusItem(element.State), getFormatedTextItem(element.Text));
+
+                index++;
+            }
+
+            Console.WriteLine("─────┴───────────┴{0}─", line);
+        }
+
+        private string getFormatedTextItem(string text) {
+            int maxWidth = Console.WindowWidth - 21;
+            if (text.Length < maxWidth) {
+                return text;
+            } else {
+                return text.Substring(0, maxWidth - 3) + "...";
+            }
+        }
+
+        private string getTextLine() {
+            int width = Console.WindowWidth - 19;
+            return "".PadLeft(width, '─');
+        }
+
+        private string getStatusItem(ElementState state) {
+            return ("[" + state + "]").PadRight(9);
+        }
+    }
+}
--- todo-cli/Program.cs
+++ todo-cli/Program.cs
@@ -4,12 +4,33 @@ using System.Collections.Generic;
 namespace todo_cli {
     class Program {
         private static CStorage Storage;
+        private static COutput Output;
+
         static void Main(string[] args) {
             Storage = new CStorage();
+            Output = new COutput();
 
-            foreach (TodoElement element in Storage.getTodoList()) {
-                Console.WriteLine(element.Text);
+            if (args.Length > 0) {
+                //switch (args[0]) {
+                //    case "add":
+                //    case "-a":
+                //    case "a":
+                //        Storage.createElement("Hello");
+                //        break;
+                //    case "delete":
+                //    case "del":
+                //    case "-d":
+                //    case "d":
+                //        Storage.deleteElement(0);
+                //        break;
+                //    default:
+                //        Output.writeList(Storage.getTodoList());
+                //        break;
+                //}
+            } else {
+                Output.writeList(Storage.getTodoList());
             }
+
         }
     }
 }
